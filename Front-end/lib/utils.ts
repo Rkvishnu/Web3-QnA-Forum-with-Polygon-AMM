e12b3184bc27499593809ee0a490956ed7b2c285
Vishnu Rathore <81683825+Rkvishnu@users.noreply.github.com>
2023-03-25 00:24:09 +0530

    Update utils.ts

--- Front-end/lib/utils.ts
+++ Front-end/lib/utils.ts
@@ -1,8 +1,7 @@
 import { BigNumber, ethers } from "ethers";
 
 // * converts the number to a BigNumber with the correct units to keep Solidity happy
-// * NOTE: Math in Solidity only uses fixed-point numbers so we use the smallest unit, i.e. n * 10^18
-// * e.g. makeBig(1) = 1000000000000000000, as 1 ETH is equal to 1000000000000000000 wei
+ 
 const makeBig = (value: string | number) => {
 
     if (typeof value == 'number') {
@@ -10,9 +9,7 @@ const makeBig = (value: string | number) => {
     }
     return ethers.utils.parseUnits(value)
 };
-
-// * converts a BigNumber to a user-friendly string for readability
-// * e.g. makeNum(1345600000000000000) = 1.34
+ 
 const makeNum = (value: BigNumber) => {
     const numStr = ethers.utils.formatUnits(value, 18);
     return numStr.substring(0, numStr.indexOf('.') + 3); //keep only 2 decimals
